d804aeff6e00d0b16283e4f36d7e8e55ab6d1afb
NierRep24 <danielsanchezmartinez064@gmail.com>
2023-05-26 10:31:51 -0600

    ADDED: se agregaron estos archivos para el examen del profesor Franklin

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+CODIGO
+<script src="script.js"></script>
+<script>
+function popup() {
+    alert("Hola profe Franklin u.u")
+}
+</script>
+<button type="button" onclick="popup()">Probar función</button>
+</script>
+
